320fa0269ce91db6e667978f5636691553cb07ac
Danyaal Akhtar <Danyaal.da80@gmail.com>
2026-06-04 23:37:52 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/uk/ac/bradford/imagegalleryweb/ImagegallerywebApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.bradford.imagegalleryweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagegallerywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagegallerywebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/bradford/imagegalleryweb/ImagegallerywebApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.bradford.imagegalleryweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagegallerywebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
